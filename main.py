d3ee338e6e81d188836232510d8133f94c02010c
Sk83r40 <proinsights.pi@gmail.com>
2025-08-04 03:40:29 -0400

    main 55

--- main.py
+++ main.py
@@ -704,6 +704,11 @@ def get_trade_log(symbol):
 def health_check():
     return jsonify({'status': 'Webhook server is running!'}), 200
 
+@app.route('/cron-ping', methods=['GET'])
+def cron_ping():
+    return "pong", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
